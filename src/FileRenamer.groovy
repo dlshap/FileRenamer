ab4b16849d68df1c73d32a18e4394a5bf23f32ef
David Shapiro <DavidS@dlsa.com>
2018-06-12 22:05:41 -0500

    Anthony Bourdain

--- src/FileRenamer.groovy
+++ src/FileRenamer.groovy
@@ -1,22 +1,22 @@
 class FileRenamer {
     static main(args) {
-        def dir = new File("D:\\My Music\\For iTunes\\Headspace\\Videos\\Getting Started")
+        def dir = new File("S:\\Movies\\Playon Server\\TV\\Parts Unknown")
         dir.eachFile {
             def path = it.getParent()
             def oldname = it.getName()
 //            if (oldname[0] == "0")
 //                newname = "5"+oldname[1..-1]
 //            else newname = "60"+oldname[2..-1]
-            def pat = /(HS )(.*)/
+            def pat = /Anthony Bourdain_ (.*)\u0024/
             def matcher = oldname =~ pat
             def newname
 //            println "b:${matcher.size()}"
             if (matcher.size() == 1) {
 //                println "s:${matcher[0].size()}"
-                if (matcher[0].size() == 3) {
-                    newname = "HS" + matcher[0][2]
+                if (matcher[0].size() == 2) {
+                    newname = matcher[0][1]
                     println "$it .renameTo(${path + "\\" + newname})"
-//                    it.renameTo("${path + "\\" + newname}")
+                    it.renameTo("${path + "\\" + newname}")
                 }
             }
         }
