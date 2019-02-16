2a27a4d2276485b458239fe1c2aa4b8b15ea3cbe
David Shapiro <DavidS@dlsa.com>
2019-02-16 17:24:24 -0600

    Lookin good

---
+++ src/FileLister.groovy
@@ -0,0 +1,38 @@
+class FileLister {
+
+    FileLister() {
+        this.start()
+    }
+
+    static main(args) {
+        new FileLister()
+    }
+
+    def start() {
+
+        def startDir = "D:\\My Videos"
+
+        println "Starting at: $startDir"
+        File fileDir = new File(startDir)
+        printdir(fileDir, 0)
+    }
+
+    def spaces(int n) {
+        def spaces = ""
+        (0..n).each {spaces += " "}
+        spaces
+    }
+
+    def printdir(File fileDir, int spaceCount) {
+        def newSpaceCount = spaceCount + 2
+        fileDir.eachFile { File eachFile ->
+            if (eachFile.isDirectory()) {
+                println "\n${spaces(newSpaceCount)}Dir: $eachFile.name"
+                printdir(eachFile, newSpaceCount)
+            } else {
+                println "${spaces(newSpaceCount)}${eachFile.name}"
+            }
+        }
+    }
+
+}
--- src/FileRenamer.groovy
+++ src/FileRenamer.groovy
@@ -1,21 +1,20 @@
 class FileRenamer {
     static main(args) {
+
         def REALRUN = true
-        def dir = new File("S:\\Movies\\Playon Server\\TV\\Anthony Bourdain - Parts Unknown")
+
+        def dir = new File("S:\\Movies\\PLEX Server\\Home Videos\\1993-1994\\")
         dir.eachFile {
             def path = it.getParent()
             def oldname = it.getName()
-//            if (oldname[0] == "0")
-//                newname = "5"+oldname[1..-1]
-//            else newname = "60"+oldname[2..-1]
-            def pat = /Anthony Bourdain_ (.*)\u0024/
+            def pat = /.*?_(\d*).mp4/
             def matcher = oldname =~ pat
             def newname
-//            println "b:${matcher.size()}"
             if (matcher.size() == 1) {
-//                println "s:${matcher[0].size()}"
-                if (matcher[0].size() == 2) {
-                    newname = matcher[0][1]
+                if (matcher[0].size() >= 2) {
+//                    def newNum = (matcher[0][1]).toInteger()+2
+//                    def newNumString = sprintf("%02d", newNum)
+                    newname = "1993-${sprintf("%02d",(matcher[0][1]).toInteger()+2)} xxx .mp4"
                     println "$it .renameTo(${path + "\\" + newname})"
                     if (REALRUN)
                         it.renameTo("${path + "\\" + newname}")
