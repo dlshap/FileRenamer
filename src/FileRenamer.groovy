38e668000ece6f857a8da6a44b0df1bc290a5803
David Shapiro <DavidS@dlsa.com>
2019-02-18 08:00:44 -0600

    Lookin good

--- src/FileRenamer.groovy
+++ src/FileRenamer.groovy
@@ -1,27 +1,50 @@
 class FileRenamer {
+
+    def REALRUN = true
+
+    def REPLACE = true
+
+    def pathName = "S:\\Movies\\PLEX Server\\Home Videos\\Home Movies\\1957-1960"
+    String path, oldname, newname
+
+    def renameWithReplace() {
+        newname = oldname.replaceAll(/1957-60/,"1957-1959")
+    }
+
+    def renameWithRegex() {
+        def pat = /(.*? ).*?_(\d*).mp4/
+        pat = /(.*?)(\d\d-)(.*)/
+        def matcher = oldname =~ pat
+        if (matcher.size() == 1) {
+            if (matcher[0].size() >= 2) {
+//                    newname = "${matcher[0][1]}${sprintf("%02d",(matcher[0][2]).toInteger()+2)}- xxx .mp4"
+                newname = "${matcher[0][1]}${matcher[0][2]}${matcher[0][3]}.mp4"
+            }
+        }
+    }
+
     static main(args) {
+        new FileRenamer()
+    }
 
-        def REALRUN = true
+    FileRenamer() {
+        start()
+    }
 
-        def pathName = "S:\\Movies\\PLEX Server\\Home Videos\\Home Movies\\1957-1960"
-
+    def start() {
         def dir = new File(pathName)
         dir.eachFile {
-            def path = it.getParent()
-            def oldname = it.getName()
-            def pat = /(.*? ).*?_(\d*).mp4/
-            def matcher = oldname =~ pat
-            def newname
-            if (matcher.size() == 1) {
-                if (matcher[0].size() >= 2) {
-//                    def newNum = (matcher[0][1]).toInteger()+2
-//                    def newNumString = sprintf("%02d", newNum)
-//                    newname = "${matcher[0][1]}${sprintf("%02d",(matcher[0][2]).toInteger()+2)} xxx .mp4"
-                    newname = "${matcher[0][1]}${sprintf("%02d",(matcher[0][2]).toInteger()+2)}- xxx .mp4"
-                    println "$it .renameTo(${path + "\\" + newname})"
-                    if (REALRUN)
-                        it.renameTo("${path + "\\" + newname}")
-                }
+            path = it.getParent()
+            oldname = it.getName()
+            newname = ""
+            if (REPLACE)
+                renameWithReplace()
+            else
+                renameWithRegex()
+            if (newname != "") {
+                println "$it .renameTo(${path + "\\" + newname})"
+                if (REALRUN)
+                    it.renameTo("${path + "\\" + newname}")
             }
         }
     }
