4701521138f97f9b784dd7ecc261545927d029ed
David Shapiro <DavidS@dlsa.com>
2020-12-10 07:26:55 -0600

    Lookin good

--- src/FileRenamer.groovy
+++ src/FileRenamer.groovy
@@ -1,10 +1,12 @@
+import groovy.io.FileType
+
 class FileRenamer {
 
     Boolean REALRUN = true
 
-    Boolean REPLACE = true
+    Boolean REPLACE = false
 
-    def pathName = "E:/Movies/PLEX Server/TV/Dr. Who/"
+    def pathName = "D:\\Plex Home Videos\\Home Movies"
 
     String path, oldname, newname
 
@@ -13,11 +15,16 @@ class FileRenamer {
     }
 
     def renameWithRegex() {
-        def pat = /(.*?)( Series 6.*?Bonus #)(\d)(.*)/
+//        def pat = /(.*?)( Series 6.*?Bonus #)(\d)(.*)/
+//        def pat = /(\d\d)-(\d\d)-(\d\d)(.*)/
+        def pat = /(\d\d)-(\d\d) ([a-z,A-Z]{3})[^ ]?(.*)?/
+        def monthNames = ["jan":"01","feb":"02","mar":"03","apr":"04","may":"05","jun":"06",
+                          "jul":"07","aug":"08","sep":"09","oct":"10","nov":"11","dec":"12"]
         def matcher = oldname =~ pat
         if (matcher.size() == 1) {
             if (matcher[0].size() >= 2) {
-                newname = "Doctor Who Bonus s06e${(sprintf("%02d", (matcher[0][3]).toInteger()))}.mp4"
+                def monthNum = monthNames[((matcher[0][3]).toString().toLowerCase())]
+                newname = "${matcher[0][1]}${monthNum}${matcher[0][2]}${matcher[0][4]}"
 //                newname = "${matcher[0][2]} ${matcher[0][1]}.mp4"
             }
         }
@@ -32,7 +39,17 @@ class FileRenamer {
     }
 
     def start() {
-        def dir = new File(pathName)
+        def topDir = new File(pathName)
+        topDir.eachFile(FileType.DIRECTORIES) {
+            def pathName = it.getParent()
+            def dirName = pathName + "/"+ it.getName()
+            renameFiles(dirName)
+        }
+    }
+
+    def renameFiles(subpath) {
+        println "${subpath}:\n\r"
+        def dir = new File(subpath)
         dir.eachFile {
             path = it.getParent()
             oldname = it.getName()
