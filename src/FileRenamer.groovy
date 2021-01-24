2cde394f5aa4b6d6bcbce36421c43291f28ca44a
David Shapiro <DavidS@dlsa.com>
2021-01-24 11:56:51 -0600

    Lookin good

--- src/FileRenamer.groovy
+++ src/FileRenamer.groovy
@@ -2,11 +2,11 @@ import groovy.io.FileType
 
 class FileRenamer {
 
-    Boolean REALRUN = true
+    Boolean REALRUN = false
 
     Boolean REPLACE = false
 
-    def pathName = "D:\\Plex Home Videos\\Home Movies"
+    def pathName = "D:\\My Music\\For iTunes\\Headspace\\Pro 1-6"
 
     String path, oldname, newname
 
@@ -20,11 +20,7 @@ class FileRenamer {
 
     def start() {
         def topDir = new File(pathName)
-        topDir.eachFile(FileType.DIRECTORIES) {
-            def pathName = it.getParent()
-            def dirName = pathName + "/"+ it.getName()
-            renameFiles(dirName)
-        }
+        renameFiles(topDir)
     }
 
     def renameFiles(subpath) {
@@ -53,15 +49,18 @@ class FileRenamer {
     def renameWithRegex() {
 //        def pat = /(.*?)( Series 6.*?Bonus #)(\d)(.*)/
 //        def pat = /(\d\d)-(\d\d)-(\d\d)(.*)/
-        def pat = /(\d\d)-(\d\d) ([a-z,A-Z]{3})[^ ]?(.*)?/
-        def monthNames = ["jan":"01","feb":"02","mar":"03","apr":"04","may":"05","jun":"06",
-                          "jul":"07","aug":"08","sep":"09","oct":"10","nov":"11","dec":"12"]
+//        def pat = /(\d\d)-(\d\d) ([a-z,A-Z]{3})[^ ]?(.*)?/
+//        def monthNames = ["jan": "01", "feb": "02", "mar": "03", "apr": "04", "may": "05", "jun": "06",
+//                          "jul": "07", "aug": "08", "sep": "09", "oct": "10", "nov": "11", "dec": "12"]
+
+        def pat = /(\d\d-Pro Level 6-)(\d{1,2})(.mp3)/
+
         def matcher = oldname =~ pat
         if (matcher.size() == 1) {
             if (matcher[0].size() >= 2) {
-                def monthNum = monthNames[((matcher[0][3]).toString().toLowerCase())]
-                newname = "${matcher[0][1]}${monthNum}${matcher[0][2]}${matcher[0][4]}"
-//                newname = "${matcher[0][2]} ${matcher[0][1]}.mp4"
+//                def monthNum = monthNames[((matcher[0][3]).toString().toLowerCase())]
+//                newname = "${matcher[0][1]}${monthNum}${matcher[0][2]}${matcher[0][4]}"
+                newname = "${matcher[0][1]}${matcher[0][2]}x${matcher[0][3]}"
             }
         }
     }
