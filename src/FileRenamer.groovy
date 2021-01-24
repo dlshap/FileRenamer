813dff49220cbc96674fbbc420fe9c681ef7a737
David Shapiro <DavidS@dlsa.com>
2021-01-24 11:48:19 -0600

    Lookin good

--- src/FileRenamer.groovy
+++ src/FileRenamer.groovy
@@ -10,26 +10,6 @@ class FileRenamer {
 
     String path, oldname, newname
 
-    def renameWithReplace() {
-        newname = oldname.replaceAll(/Bonus s06/, "Bonus s01")
-    }
-
-    def renameWithRegex() {
-//        def pat = /(.*?)( Series 6.*?Bonus #)(\d)(.*)/
-//        def pat = /(\d\d)-(\d\d)-(\d\d)(.*)/
-        def pat = /(\d\d)-(\d\d) ([a-z,A-Z]{3})[^ ]?(.*)?/
-        def monthNames = ["jan":"01","feb":"02","mar":"03","apr":"04","may":"05","jun":"06",
-                          "jul":"07","aug":"08","sep":"09","oct":"10","nov":"11","dec":"12"]
-        def matcher = oldname =~ pat
-        if (matcher.size() == 1) {
-            if (matcher[0].size() >= 2) {
-                def monthNum = monthNames[((matcher[0][3]).toString().toLowerCase())]
-                newname = "${matcher[0][1]}${monthNum}${matcher[0][2]}${matcher[0][4]}"
-//                newname = "${matcher[0][2]} ${matcher[0][1]}.mp4"
-            }
-        }
-    }
-
     static main(args) {
         new FileRenamer()
     }
@@ -62,6 +42,26 @@ class FileRenamer {
                 println "$it renamed to ${path + "/" + newname}"
                 if (REALRUN)
                     it.renameTo("${path + "/" + newname}")
+            }
+        }
+    }
+
+    def renameWithReplace() {
+        newname = oldname.replaceAll(/Bonus s06/, "Bonus s01")
+    }
+
+    def renameWithRegex() {
+//        def pat = /(.*?)( Series 6.*?Bonus #)(\d)(.*)/
+//        def pat = /(\d\d)-(\d\d)-(\d\d)(.*)/
+        def pat = /(\d\d)-(\d\d) ([a-z,A-Z]{3})[^ ]?(.*)?/
+        def monthNames = ["jan":"01","feb":"02","mar":"03","apr":"04","may":"05","jun":"06",
+                          "jul":"07","aug":"08","sep":"09","oct":"10","nov":"11","dec":"12"]
+        def matcher = oldname =~ pat
+        if (matcher.size() == 1) {
+            if (matcher[0].size() >= 2) {
+                def monthNum = monthNames[((matcher[0][3]).toString().toLowerCase())]
+                newname = "${matcher[0][1]}${monthNum}${matcher[0][2]}${matcher[0][4]}"
+//                newname = "${matcher[0][2]} ${matcher[0][1]}.mp4"
             }
         }
     }
